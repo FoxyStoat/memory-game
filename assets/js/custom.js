d8ea095c70365e07199b8587b2ce8d03b1a6977a
FoxyStoat <carol.hodgson@hotmail.com>
2018-04-09 18:35:09 +0100

    add array for deck of cards

---
+++ assets/js/custom.js
@@ -0,0 +1,3 @@
+// Array of Deck of Cards
+
+const deck = ["fa-anchor", "fa-anchor", "fa-bicycle", "fa-bicycle", "fa-bolt", "fa-bolt", "fa-bomb", "fa-bomb", "fa-cube", "fa-cube", "fa-diamond", "fa-diamond", "fa-leaf", "fa-leaf", "fa-paper-plane-o", "fa-paper-plane-o"];
