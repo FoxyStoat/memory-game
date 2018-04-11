50fd27dd18c93135857eec1851d6e5638270ba05
FoxyStoat <carol.hodgson@hotmail.com>
2018-04-11 16:36:21 +0100

    amend array with img classes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,3 @@
 // Array of Deck of Cards
 
-const deck = ["fa-anchor", "fa-anchor", "fa-bicycle", "fa-bicycle", "fa-bolt", "fa-bolt", "fa-bomb", "fa-bomb", "fa-cube", "fa-cube", "fa-diamond", "fa-diamond", "fa-leaf", "fa-leaf", "fa-paper-plane-o", "fa-paper-plane-o"];
+let deck = ["agility", "agility", "boat", "boat", "citizenship", "citizenship", "hack", "hack", "nerd-rage", "nerd-rage", "nuka-cola", "nuka-cola", "robotics", "robotics", "shocked", "shocked"];
