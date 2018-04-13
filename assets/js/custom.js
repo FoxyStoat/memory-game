2afff6590d6d2084f332d4a606629ce2ab53f429
FoxyStoat <carol.hodgson@hotmail.com>
2018-04-13 15:33:51 +0100

    amend typo in array

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,10 +1,9 @@
 // Array of Deck of Cards
-
 let deck = ["agility", "agility", "boat", "boat", "citizenship", "citizenship", "hack", "hack", "nerd-rage", "nerd-rage", "nuka-cola", "nuka-cola", "robotics", "robotics", "shocked", "shocked"];
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(deck) {
-    var currentIndex = deck.length, temporaryValue, randomIndex;
+    let currentIndex = deck.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
