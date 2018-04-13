e2a6d285beece95f57b62ef59be401ba42c3e693
FoxyStoat <carol.hodgson@hotmail.com>
2018-04-13 14:18:33 +0100

    add array shuffle function

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,21 @@
 // Array of Deck of Cards
 
 let deck = ["agility", "agility", "boat", "boat", "citizenship", "citizenship", "hack", "hack", "nerd-rage", "nerd-rage", "nuka-cola", "nuka-cola", "robotics", "robotics", "shocked", "shocked"];
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(deck) {
+    var currentIndex = deck.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = deck[currentIndex];
+        deck[currentIndex] = deck[randomIndex];
+        deck[randomIndex] = temporaryValue;
+    }
+
+    return deck;
+}
+
+shuffle(deck);
+console.log(deck);
