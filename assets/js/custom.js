9644f04de2f58dbf537c5649685b19fa8d244fab
FoxyStoat <carol.hodgson@hotmail.com>
2018-04-15 15:00:18 +0100

    amend shuffle function

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,5 @@
 // Array of Deck of Cards
-let deck = ["agility", "agility", "boat", "boat", "citizenship", "citizenship", "hack", "hack", "nerd-rage", "nerd-rage", "nuka-cola", "nuka-cola", "robotics", "robotics", "shocked", "shocked"];
+const deck = ["agility", "agility", "boat", "boat", "citizenship", "citizenship", "hack", "hack", "nerd-rage", "nerd-rage", "nuka-cola", "nuka-cola", "robotics", "robotics", "shocked", "shocked"];
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
