18e5d648d98394119a78411541aa96b45a781326
FoxyStoat <carol.hodgson@hotmail.com>
2018-04-15 21:28:44 +0100

    change array from classes to image.png

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,5 @@
 // Array of Deck of Cards
-const deck = ["agility", "agility", "boat", "boat", "citizenship", "citizenship", "hack", "hack", "nerd-rage", "nerd-rage", "nuka-cola", "nuka-cola", "robotics", "robotics", "shocked", "shocked"];
+const deck = ["Agility.png", "Agility.png", "Boat.png", "Boat.png", "Citizenship.png", "Citizenship.png", "Hack.png", "Hack.png", "Nerd-Rage.png", "Nerd-Rage.png", "Nuka-Cola.png", "Nuka-Cola.png", "Robotics.png", "Robotics.png", "Shock.png", "Shock.png"];
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -13,6 +13,7 @@ function shuffle(array) {
         array[randomIndex] = temporaryValue;
     }
     return array;
+
 }
 shuffle(deck);
 console.log(deck);
